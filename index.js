0e0f3ba57d8ba84ff305ddc53d68bba9c1e8cc62
Kevin Thizy <kevin.thizy@intech.lu>
2019-11-20 15:23:50 +0100

    Create server

---
+++ index.js
@@ -0,0 +1,23 @@
+// Import library
+const express = require('express');
+// Use default PORT or given by platform.
+const PORT = process.env.PORT || 80;
+
+// Create application
+const app = express();
+
+// Some basic route
+app.get('/', (req, res) => {
+  // Send response.
+  res.json({ message: 'coucou' });
+});
+
+// Start server.
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.info('Server started.');
+});
